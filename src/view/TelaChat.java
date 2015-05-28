c46006ad642b4a6abc2a4d28dad1c57cccbcc8d6
Gustavo <luis.schuler@gmail.com>
2015-05-28 16:35:11 -0300

    Algumas alterações no envio de arquivo

--- src/view/TelaChat.java
+++ src/view/TelaChat.java
@@ -39,6 +39,25 @@ import org.json.JSONObject;
 
 import controller.EventosDoServidorDeSockets;
 
+/*
+ No seu form de chat, implemente a interface IFileDownloadHandler.java.
+
+Utilize a thread FileSender.java para enviar um arquivo.
+
+- hostAddress: o endereço do novo socket de envio de arquivo.
+- port: a porta do novo socket de envio de arquivo.
+- filePathToSend: o caminho do arquivo a ser enviado.
+- fileDownloadHandler: o form que implementou ``IFileDownloadHandler.java``
+
+Utilize a thread FileReceiver.java para receber um arquivo.
+
+- socket: o novo socket para recebimento do arquivo.
+- fileSize: o tamanho do arquivo que está sendo enviado.
+- filePathToSave: o caminho onde deve salvar o arquivo.
+- fileDonwloadHandler: o form que implementou ``IFileDownloadHandler.java``
+
+ */
+
 public class TelaChat extends JFrame implements WindowListener, controller.EventosDoServidorDeSockets, controller.IFileDownloadHandler {
 	
 	private Socket socket;
@@ -262,6 +281,7 @@ public class TelaChat extends JFrame implements WindowListener, controller.Event
 	}
 	
 	private void aceitaEnvioArquivo(){
+
 		try {
 			OutputStream os = socket.getOutputStream();
 			DataOutputStream dos = new DataOutputStream( os );
@@ -273,14 +293,16 @@ public class TelaChat extends JFrame implements WindowListener, controller.Event
 			transacao.put( "porta", nroPorta );
 			
 			dos.writeUTF( transacao.toString() );
-			
+			//controller.FileReceiver receiver = new controller.FileReceiver();
 			
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog( this, "Não foi possível atender sua requisição: " + e.getMessage() );
 		}
+		
 	}
 	
 	private void recusaEnvioArquivo(){
+		
 		try {
 			OutputStream os = socket.getOutputStream();
 			DataOutputStream dos = new DataOutputStream( os );
